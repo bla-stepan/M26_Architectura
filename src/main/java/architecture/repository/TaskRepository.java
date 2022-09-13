8327804af526b7ba8eb0731afab022c7dc7c9af5
Stepan <bla-stepan@mail.ru>
2022-09-13 16:00:57 +0300

    Модуль 26.8 добавлен тест проверки сохранения изменения задачи

--- src/main/java/architecture/repository/TaskRepository.java
+++ src/main/java/architecture/repository/TaskRepository.java
@@ -1,17 +1,53 @@
 package architecture.repository;
 
 import architecture.model.Task;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class TaskRepository {
 
+    private static AtomicLong ID_COUNTER = new AtomicLong();
     //В данном приложении для простоты будем использовать List для хранения задачек
 
     private List<Task> taskList;
 
-    public TaskRepository(){
+    public TaskRepository() {
         this.taskList = new ArrayList<>();
+    }
+
+    public boolean add(Task task) {
+        //наверное загрузка в лист объекта таск
+        //у задачек должны быть IDишники. добавим счетчик, который будет считать IDшки
+        task.setId(ID_COUNTER.incrementAndGet());
+        return taskList.add(task);
+    }
+
+    public Task getTaskById(Long id) {
+        if (id == null) {
+            throw new RuntimeException("Не был передан ID");//не лучшая практика так делать (следует сделать свое исключение, например TaskNotFoundException)
+        }
+
+        return taskList.stream()
+                .filter(saveTask -> saveTask.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Указанная задача не найдена"));
+        //пора запустить наш первый тест!
+    }
+
+    public void editTask(Task task) {
+        if(task==null){
+            throw  new RuntimeException("Не удалось найти такую задачу");
+        }
+
+        Task savedTask = taskList.stream()
+                .filter(editTask -> editTask.equals(task))
+                .findFirst()
+                .orElseThrow(()-> new RuntimeException("Не удалось найти задачу"));
+        //Задачу мы нашли теперь нужно обновить ее все поля, кроме ID
+
+        savedTask.setAssignTo(task.getAssignTo());
+        savedTask.setAuthor(task.getAuthor());
+        savedTask.setDone(task.isDone());
     }
 }
--- src/test/java/architecture/repository/TaskRepositoryTest.java
+++ src/test/java/architecture/repository/TaskRepositoryTest.java
@@ -1,8 +1,70 @@
 package architecture.repository;
 
+import architecture.model.Task;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class TaskRepositoryTest {
 
-    //Начнем с уровня доступа к базе данных
+    //Начнем с уровня доступа к базе данных. Основные операции - это добавление/удаление/редактирование и получение задачек.
+
+    @Test
+    public void whenAddTaskShouldCheckThatTaskWasAdded(){
+        //название метода - при Добавлении Задачи Следует Проверить, Что Задача Была Добавлена
+        TaskRepository taskRepository = new TaskRepository();
+        Task task = new Task();
+        task.setDone(false);
+        task.setAssignTo("SkillFactory Student");
+        task.setAuthor("Blandinskiy Stepan");
+        boolean added = taskRepository.add(task);//требует реализации в классе таскрепозиторий
+        //мы добавили задачку, теперь надо проверить что она действительно сохранилась
+        //для этого нам нужно получить только, что добавленную задачку по ID
+
+        Task findByIdTask = taskRepository.getTaskById(1L);//требуется реализация в классе таксрепозиторий
+        //TDD гласит, что в тестах должно быть 3А - Assing, action, assert (назначение, действие, утверждение)
+        //Самое время для assert (утверждения)
+        assertTrue(added);
+        assertNotNull(findByIdTask);
+        assertNotNull(findByIdTask.getId());
+        assertThat("Не верное назначение для", findByIdTask.getAssignTo(), is("SkillFactory Student"));
+        assertThat("Не верный автор", findByIdTask.getAuthor(), is("Blandinskiy Stepan"));
+        assertThat("Не верная отметка о выполнении", findByIdTask.isDone(), is(false));
+    }
+    //тест прошел, но необходимо добавить еще функции - такие как редактирование/удаление и получение всего списка задач
+    @Test
+    public void whenEditTaskShouldCheckThatChangesSaved(){
+        //название метода - при редактировании задачи следует проверить что изменения сохранены
+        TaskRepository taskRepository = new TaskRepository();
+        Task task = new Task();
+        task.setDone(true);
+        task.setAuthor("Blandinskiy Stepan");
+        task.setAssignTo("SkillFactory Student");
+        boolean added = taskRepository.add(task); //да первый тест нужно немного отредактировать
+
+        Task findByIdTask = taskRepository.getTaskById(1L);
+
+        //Сейчас нужно скопировать проверки из первого теста
+        //в копи-пасте нет ничего страшного если она осмысленная
+        //в данном случае лишняя проверка не помешает
+        assertTrue(added);
+        assertNotNull(findByIdTask);
+        assertNotNull(findByIdTask.getId());
+        assertThat("Не верное назначение для", findByIdTask.getAssignTo(), is("SkillFactory Student"));
+        assertThat("Не верный автор", findByIdTask.getAuthor(), is("Blandinskiy Stepan"));
+        assertThat("Не верная отметка о выполнении", findByIdTask.isDone(), is(false));
+
+        //К этому моменту мы уже точно знаем, что задачка хранится в условной БД
+        //ОТредатируем бд
+        findByIdTask.setAssignTo("Blandinskiy Stepan");
+        taskRepository.editTask(findByIdTask);
+
+        //теперь проверим, что изменения действительно сохранились
+        findByIdTask = taskRepository.getTaskById(1L);
+
+        assertNotNull(findByIdTask);
+        assertThat("Неверное назначение для", findByIdTask.getAssignTo(), is("Blandinskiy Stepan"));
+
+    }
 }
